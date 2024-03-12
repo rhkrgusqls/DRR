b9cbba99080d1d0128f513d1780ba6000bb9977b
rhkrgusqls <79958305+rhkrgusqls@users.noreply.github.com>
2024-03-12 13:09:27 +0900

    AIBase
    AiController 생성
    BehaviorTree 생성 및 기본 틀 제작
    BlackBoard 생성 및 기본 변수 생성

---
+++ Source/DRR/Public/EnemyCharacter/AIController/NormalAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "NormalAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRR_API ANormalAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
