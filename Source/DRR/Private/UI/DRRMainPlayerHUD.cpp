3f24cf5352f07bb7155fa7dc9d083235d919ecf4
bakkuttullu <pulleroco0779@naver.com>
2024-03-28 06:03:46 +0900

    C++ HUD Class Created

---
+++ Source/DRR/Private/UI/DRRMainPlayerHUD.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/DRRMainPlayerHUD.h"
+
+ADRRMainPlayerHUD::ADRRMainPlayerHUD()
+{
+	/*
+	static ConstructorHelpers::FClassFinder<ADRRMainPlayerHUD> DRRHUDRef(TEXT("/Game/Blueprints/UI/BP_HUD.BP_HUD_C"));
+	if (DRRHUDRef.Class)
+	{
+		//MainHUD = DRRHUDRef.Class;
+	}
+	*/
+}
---
+++ Source/DRR/Public/UI/DRRMainPlayerHUD.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "DRRMainPlayerHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRR_API ADRRMainPlayerHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	ADRRMainPlayerHUD();
+
+protected:
+	//TSubclassOf<UUserWidget> MainHUDWidgetClass;
+	
+};
